9c34a208eedf745f390ac915b0a7dae97e3447b6
Chonathun Cheepsathis <cchnthay@gmail.com>
2026-06-30 11:29:58 +0700

    feat: add 192x192 and 512x512 icon sizes to PWA manifest configuration

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,16 @@ export default defineConfig({
         icons: [
           {
             src: 'favicon.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'favicon.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'favicon.svg',
             sizes: 'any',
             type: 'image/svg+xml',
             purpose: 'any maskable'
